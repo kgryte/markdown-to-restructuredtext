84f2912f46820e44fb9cb4c522cb38a7c0206d06
kgryte <kgryte@gmail.com>
2016-02-13 23:16:46 -0800

    Add validation tests and fix validation bug

---
+++ test/test.validate.js
@@ -0,0 +1,87 @@
+'use strict';
+
+// MODULES //
+
+var tape = require( 'tape' );
+var validate = require( './../lib/validate.js' );
+
+
+// TESTS //
+
+tape( 'file exports a validation function', function test( t ) {
+	t.equal( typeof validate, 'function', 'file exports a function' );
+	t.end();
+});
+
+tape( 'if an options argument is not an object, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		'5',
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, values[i] );
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'if provided a `flavor` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'flavor': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
+tape( 'the function returns `null` if all options are valid', function test( t ) {
+	var err;
+
+	err = validate( {}, {
+		'flavor': 'github'
+	});
+	t.equal( err, null, 'returns null' );
+
+	t.end();
+});
+
+tape( 'the function ignores unrecognized options', function test( t ) {
+	var err;
+
+	err = validate( {}, {
+		'beep': 'boop',
+		'a': 5,
+		'b': null,
+		'c': 'woot'
+	});
+	t.equal( err, null, 'returns null' );
+
+	t.end();
+});
